cdfa7b0d1d625602bb5c5311623d9f1531eabf0d
Kenneth <kenm.riv@gmail.com>
2021-02-21 16:43:59 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment1QA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
